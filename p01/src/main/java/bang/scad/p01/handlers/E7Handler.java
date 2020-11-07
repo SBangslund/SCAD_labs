e0d47cacbd15784b289f7ffcf9e5ead45bbb49ef
Samuel Bangslund <s.bangslund@hotmail.dk>
2020-11-07 01:43:12 +0100

    Added template for all other cases (e1, e3, e7)

--- p01/src/main/java/bang/scad/p01/handlers/E7Handler.java
+++ p01/src/main/java/bang/scad/p01/handlers/E7Handler.java
@@ -1,14 +1,14 @@
 package bang.scad.p01.handlers;
 
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import java.util.Map;
-
-public class E2Handler implements RequestHandler<Map<String, String>, String> {
+public class E7Handler implements RequestHandler<Map<String, String>, String> {
 
     @Override
     public String handleRequest(Map<String, String> input, Context context) {
-        return "e2";
+        return "e7";
     }
 }
